b7832a99edc0f8897a3600e949e637dd01e6c865
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-07 15:04:16 -0500

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,18 @@
+#! / usr / bin / bash
+
+apt-get update
+actualización de apt-get
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+wget -O ~ /../usr/share/tessdata/ind.traineddata " https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true "
+corrección de auditoría npm
+npm i imgbb-uploader
+npm cache clean -f
+npm install --dev
+npm tengo
+
+echo  " [*] Se han instalado todas las dependencias, ejecute el comando \" npm start \ " para iniciar inmediatamente el script "
+echo  " Actualizaciones: corregir errores "
