ceb1515d2e2cf9040e920692fe1ef3fa09e7bd8a
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:48:56 -0500

    Create fsmenu.js

---
+++ src/fsmenu.js
@@ -0,0 +1,13 @@
+const  nsfwmenu  =  ( prefijo ,  pushname )  =>  {
+    return  `* Comandos para ver pornito 🔞 *
+* NSFW ✅ *
+- $ { prefijo } nsfwbobs
+- $ { prefijo } nsfwsidebobs
+- $ { prefijo } nsfwahegao
+- $ { prefijo } nsfwfeets
+_El bot nesecita admin y tener activado los NSFW_ \ n _Digita_ \ n $ { prefix } * nsfw 1 *
+Algunas funciones fueron eliminadas por errores en el servidor de la India
+ву ѕнαη∂υу`
+
+}
+
