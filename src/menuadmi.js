60c6093a15c40a5c96baf3c0f87281383eb7bcfb
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:47:58 -0500

    Create menuadmi.js

---
+++ src/menuadmi.js
@@ -0,0 +1,33 @@
+const  menuadmin  =  ( prefijo ,  pushname )  =>  {
+ return  `Hola aqui los comandos para admins
+ _Para que el bot ejecute estos comandos obs es necesario dar admin al bot_
+ 
+ ◪ * COMANDOS DE ADMIN *
+ │
+├─ ❏ $ { prefijo } opengc
+├─ ❏ $ { prefijo } closegc
+├─ ❏ $ { prefix } promover
+├─ ❏ $ { prefix } degradar
+├─ ❏ $ { prefix } tagall
+├─ ❏ $ { prefix } tagall2
+├─ ❏ $ { prefix } tagall3
+├─ ❏ $ { prefix } tagall4
+├─ ❏ $ { prefix } tagall5
+├─ ❏ $ { prefix } agregar
+├─ ❏ $ { prefijo } patada
+├─ ❏ $ { prefix } listadmins
+├─ ❏ $ { prefix } linkgroup
+├─ ❏ $ { prefix } dejar
+├─ ❏ $ { prefix } bienvenido
+├─ ❏ $ { prefijo } nsfw
+├─ ❏ $ { prefix } nivelación
+├─ ❏ $ { prefix } nivel
+├─ ❏ $ { prefix } eliminar
+├─ ❏ $ { prefijo } simih
+└─ ❏ $ { prefix } ownergroup
+ '
+
+
+}
+
+Exportaciones . menuadmin  =  menuadmin
