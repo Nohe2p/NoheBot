91d809c7bb3c3dec3f20e251a7bd2076b9731c11
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:52:40 -0500

    Create toinmenu.js

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const  toinmenu  =  ( prefijo ,  pushname )  =>  {
+    volver  `◪ * Comandos de Nohe *
+    │
+    ├─ ❏ $ { prefix } setprefix
+    ├─ ❏ $ { prefijo } bloque
+    ├─ ❏ $ { prefijo } bc
+    ├─ ❏ $ { prefijo } bcgc
+    └─ ❏ $ { prefijo } clearall`
+
+}
+
+Exportaciones . toinmenu  =  toinmenu
