a91d698fdd73c45338f287a563bd30e6c5664559
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:50:02 -0500

    Create otaku.js

---
+++ src/otaku.js
@@ -0,0 +1,13 @@
+const  otaku  =  ( prefijo ,  pushname )  =>  {
+    return  `* Palabras especificas para que el bot interactue con ustedes mis queridos otakus *
+Quien es tu sempai botsito
+Yo gimes 7u7
+Te amo botsito uwu
+Onichan
+La toca 7w7
+_ * Ojito escribe tal como esta * _
+ву ѕнαη∂υу`
+
+}
+
+Exportaciones . otaku  =  otaku
