ee6d52c1a4847b9832dc0ee51d8a1b9a44669f6f
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:22:02 -0500

    Create bahasa.js

---
+++ src/bahasa.js
@@ -0,0 +1,56 @@
+const  bahasa  =  ( prefijo ,  remitente )  =>  {
+	volver  `* Lista De Idiomas Para El Bot *
+	* Codigos Idiomas *
+    sq albanés
+    ar árabe
+    hy armenio
+    ca catalán
+    zh chino
+    zh-cn chino (China)
+    zh-tw chino (Taiwán)
+    zh-yue chino (cantonés)
+    hr croata
+    cs checo
+    da danés
+    nl holandés
+    en ingles
+    en-au inglés (Australia)
+    en-uk inglés (Reino Unido)
+    en-us inglés (Estados Unidos)
+    eo esperanto
+    fi finlandés
+    fr francés
+    de alemán
+    el griego
+    ht criollo haitiano
+    hola hindi
+    hu húngaro
+    es islandés
+    id indonesio
+    es italiano
+    ja japonés
+    ko coreano
+    la latina
+    lv letón
+    mk macedonio
+    no noruego
+    pl polaco
+    pt portugués
+    pt-br portugués (Brasil)
+    ro rumano
+    ru ruso
+    sr serbio
+    sk eslovaco
+    es español
+    es-es español (España)
+    es-us español (Estados Unidos)
+    sw swahili
+    sv sueco
+    ta Tamil
+    th tailandés
+    tr turco
+    vi vietnamita
+    cy Welsh`
+}
+
+Exportaciones . bahasa  =  bahasa
