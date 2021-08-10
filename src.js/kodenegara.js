175183c677ddc31cc1cade6b3bca8b73a6b0e80d
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:45:18 -0500

    Create kodenegara.js

---
+++ src.js/kodenegara.js
@@ -0,0 +1,510 @@
+const  negara  =  ( prefijo ,  remitente )  =>  {
+	return  `* Codigos Numericos
+Afganistán 93
+
+Afrika Selatan 27
+
+Afrika Tengah 236
+
+Albania 355
+
+Argelia (Aljazair) 213
+
+Amerika Serikat 1
+
+Andorra 376
+
+Angola 244
+
+Anguila 1-264
+
+Antigua 1-268
+
+Antillen Belanda 599
+
+Arabia Saudita 966
+
+Argentina 54
+
+Armenia 374
+
+Aruba 297
+
+Australia 61
+
+Austria 43
+
+Azerbaiyán 994
+
+*B*
+
+Bahama 1-242
+
+Bahréin 973
+
+Bangladesh 880
+
+Barbados 1-246
+
+Barbuda 1-268
+
+Belanda 31
+
+Bielorrusia 375
+
+Bélgica 32
+
+Belice 501
+
+Benin 229
+
+Bermudas 1-441
+
+Bután 975
+
+Bolivia 591
+
+Bosnia y Herzegovina 387
+
+Botswana 267
+
+Brasil 55
+
+Brunei Darussalam 673
+
+Bulgaria 359
+
+Burkina Faso 226
+
+Burundi 257
+
+*C*
+
+Cabo Verde 238
+
+Ceko 420
+
+Chad 235
+
+Chile 56
+
+China 86
+
+China Makau 853
+
+*D*
+
+Dinamarca 45
+
+Djibouti 253
+
+Domikia 1-767
+
+*MI*
+
+Ecuador 593
+
+El Salvador 503
+
+Eritrea 291
+
+Estonia 372
+
+Etiopía 251
+
+*F*
+
+Fiyi 679
+
+Filipina 63
+
+Finlandia 358
+
+*GRAMO*
+
+Gabón 241
+
+Gambia 220
+
+Georgia 995
+
+Ghana 233
+
+Gibraltar 350
+
+Groenlandia 299
+
+Granada 1-473
+
+Guam 1-671
+
+Guatemala 502
+
+Guinea 224
+
+Guinea Bissau 245
+
+Guinea Ekuator 240
+
+Guyana 592
+
+* H *
+
+Haití 509
+
+Honduras 504
+
+Hongaria 36
+
+Hong Kong 852
+
+*I*
+
+Indonesia 62
+
+India 91
+
+Inggris (Britania Raya) 44
+
+Irak 964
+
+Irán 98
+
+Irlanda 353
+
+Islandia 354
+
+Israel 972
+
+Italia 39
+
+* J *
+
+Jamaika 1-876
+
+Jepang 81
+
+Jerman 49
+
+Jersey 44-1534
+
+* K *
+
+Kamboja 855
+
+Kamerun 237
+
+Canadá 1
+
+Kazajstán 7
+
+Kenia 254
+
+Kepulauan Marshall 692
+
+Kepulauan Turks dan Caicos 1-649
+
+Kirguistán 996
+
+Kiribati 686
+
+Kolombia 57
+
+Komoros 682
+
+Corea Selatan 82
+
+Corea Utara 850
+
+Kosta Rika 506
+
+Kroasia 385
+
+Kuba 53
+
+Kuwait 965
+
+Kurakao 599
+
+* L *
+
+Laos 856
+
+Letonia 371
+
+Líbano 961
+
+Lesoto 266
+
+Liberia 231
+
+Libia 218
+
+Liechtenstein 423
+
+Lituania 370
+
+Luksemburg 352
+
+METRO
+
+Madagaskar 261
+
+Macedonia 389
+
+Maladewa 960
+
+Malaui 265
+
+Malasia 60
+
+Malí 223
+
+Malta 356
+
+Maroko 212
+
+Mauritania 222
+
+Mauricio 230
+
+Mayotte 262
+
+Meksiko 52
+
+Mesir 20
+
+Mikronesia 691
+
+Moldavia 373
+
+Monako 377
+
+Mongolia 976
+
+Montenegro 382
+
+Mozambik 258
+
+Myanmar 95
+
+*NORTE*
+
+Namibia 264
+
+Nauru 674
+
+Nepal 977
+
+Níger 227
+
+Nigeria 234
+
+Nikaragua 505
+
+Niue 683
+
+Noruega 47
+
+* O *
+
+Omán 968
+
+*PAG*
+
+Pakistán 92
+
+Palaos 680
+
+Palestina 970
+
+Panamá 507
+
+Pantai gading 225
+
+Papua Nugini 675
+
+Paraguay 595
+
+Perancis 33
+
+Perancis Polinesia 689
+
+Perú 51
+
+Pitcairn 64
+
+Polonia 48
+
+Portugal 351
+
+Puerto Riko 1-787, 1-939
+
+Pulau Cocos 61
+
+Cocinero Pulau 682
+
+Pulau Falkland 500
+
+Feroe de Pulau 298
+
+Hombre Pulau 44-1624
+
+Pulau Mariana Utara 1-670
+
+Pulau Reuni 262
+
+Pulau Solomon 677
+
+Virgen Pulau 1-340
+
+* Q *
+
+Qatar 974
+
+* R *
+
+Republik Ceko 420
+
+República Demokrasi Kongo 243
+
+Republik Dominika 1-809, 1-829, 1-849
+
+Rumania 40
+
+Rusia 7
+
+Ruanda 250
+
+*S*
+
+Sahara Barat 212
+
+Santo Bartolomé 590
+
+Santo Helena 290
+
+Santo Kitts y Nevis 1-869
+
+Santo Lucía 1-758
+
+Santo Vicente y las Granadinas 1-784
+
+Samoa 685
+
+San Marino 378
+
+Santo Tomé y Príncipe 239
+
+Selandia Baru 64
+
+Senegal 221
+
+Serbia 381
+
+Seychelles 248
+
+Sierra Leona 232
+
+Singapura 65
+
+Siprus 357
+
+Eslovenia 386
+
+Slowakia 421
+
+Somalia 252
+
+Spanyol 34
+
+Sri Lanka 94
+
+Sudán 249
+
+Sudán 211
+
+Suriah 963
+
+Surinam 597
+
+Svalbard dan Jan Mayen 47
+
+Swazilandia 268
+
+Swedia 46
+
+Suiza 41
+
+* T *
+
+Tayikistán 992
+
+Tanjung Verde 238
+
+Tanzania 255
+
+Taiwán 886
+
+Tailandia 66
+
+Timor Oriental 670
+
+Togo 228
+
+Tokelau 690
+
+Tonga 676
+
+Trinidad y Tobago 1-868
+
+Túnez 216
+
+Turki 90
+
+Turkmenistán 993
+
+Tuvalu 688
+
+* U *
+
+Uganda 256
+
+Ucrania 380
+
+Uni Emirat Arab 971
+
+Uruguay 598
+
+Uzbekistán 998
+
+* V *
+
+Vanuatu 678
+
+Vaticano 379
+
+Venezuela 58
+
+Vietnam 84
+
+* W *
+
+Paredes dan Futuna 681
+
+* Y *
+
+Yaman 967
+
+Yunani 30
+
+Yordania 962
+
+* Z *
+
+Zambia 260
+
+Zimbabue 263`
+}
+
+Exportaciones . negara  =  negara
