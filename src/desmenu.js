504bde68081aeea3f519010157612d8131b82918
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:23:15 -0500

    Create desmenu.js

---
+++ src/desmenu.js
@@ -0,0 +1,23 @@
+const  desmenu  =  ( prefijo ,  pushname )  =>  {
+    volver  '
+* Comandos De Descargas ✅ *
+ 
+══════════════
+*AUDIO*
+$ { prefix } play2
+Una alternativa por si * play llego a su limite de canciones
+$ { prefix } jugar
+Coloca el nombre de la cancion, o el link del video YT
+$ { prefix } reproducir https://www.youtube.com/xxxxxxxxxxxxx
+══════════════
+* COMUNICADO *
+* ytmp4 y * ytmp3 estan en mantenimiento, muy pronto sacare una actualización de esos comandos mas renovados :)
+══════════════
+    
+_ * play y * play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
+     
+ву ѕнαη∂υу`
+
+}
+
+Exportaciones . desmenu  =  desmenu
