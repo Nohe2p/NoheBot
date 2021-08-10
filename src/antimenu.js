25d16ec5060c5fa81792e9d2931a812b2fd1e2c2
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:17:54 -0500

    Create antimenu.js

---
+++ src/antimenu.js
@@ -0,0 +1,40 @@
+const  antimenu  =  ( prefijo ,  pushname )  =>  {
+    volver  '
+* Comandos Para Activar Los Antilinks 📢 *
+══════════════
+_ * Antilink De Grupos De Whatsapp * _
+* PARA ACTIVAR EL ANTILINK *
+$ { prefix } antienlace 1
+* PARA DESACTIVAR EL ANTILINK *
+$ { prefix } antienlace 0
+══════════════
+_ * Antienlace de Instagram * _
+* PARA ACTIVAR EL ANTILINK *
+$ { prefijo } antinsta 1
+* PARA DESACTIVAR EL ANTILINK *
+$ { prefix } antinsta 0
+══════════════
+_ * Antienlace de Facebook * _
+* PARA ACTIVAR EL ANTILINK *
+$ { prefix } antiface 1
+* PARA DESACTIVAR EL ANTILINK *
+$ { prefix } antiface 0
+══════════════
+_ * Antienlace De Tik Tok * _
+* PARA ACTIVAR EL ANTILINK *
+$ { prefix } antitik 1
+* PARA DESACTIVAR EL ANTILINK *
+$ { prefix } antitik 0
+══════════════
+_ * Antienlace de Youtube * _
+* PARA ACTIVAR EL ANTILINK *
+$ { prefix } antitube 1
+* PARA DESACTIVAR EL ANTILINK *
+$ { prefix } antitube 0
+_Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
+ву ѕнαη∂υу
+'
+
+}
+
+Exportaciones . antimenu  =  antimenu
