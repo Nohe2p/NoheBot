def29e53bd716d8dd809b0f36658b7c4df684ec1
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:46:43 -0500

    Create levelmenu.js

---
+++ src/levelmenu.js
@@ -0,0 +1,22 @@
+const  levelmenu  =  ( prefijo ,  pushname )  =>  {
+    volver  '
+* Comandos De Level 🆕 beta *
+ 
+══════════════
+PARA ACTIVARLO EL BOT NECESITA DE ADMINISTRACIÓN
+* PARA NIVEL ACTIVAR *
+* nivelación 1
+* NIVEL PARA DESACTIVAR *
+* nivelación 0
+══════════════
+PARA VER TU XP EN TIEMPO REAL
+*nivel
+══════════════
+    
+* Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0 *
+    
+ву ѕнαη∂υу
+'
+
+}
+
