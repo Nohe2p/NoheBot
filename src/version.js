104c703bdbe9b2115d9854564575f86bb8215689
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:53:29 -0500

    Create version.js

---
+++ src/version.js
@@ -0,0 +1,18 @@
+const  version  =  ( prefix ,  pushname )  =>  {
+    volver  '
+* ѕнαηвσт ву ѕнαη∂υу *
+* Actualizado: * 08 de Agosto del 2021
+* Versión actual: * 3.4
+* Ofrecida por: * ѕнαη∂υу ™
+* INFORMAR *
+Si no funciona el comando * play o * play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando * creador
+* BLOG DE SHANBOT *
+https://cutt.ly/jmSF2RN
+ву ѕнαη∂υу
+'
+
+}
+
