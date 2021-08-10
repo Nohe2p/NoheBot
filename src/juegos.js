1ecebe9eb1470219ee50c4d9814c7a4b523de53f
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:26:50 -0500

    Create juegos.js

---
+++ src/juegos.js
@@ -0,0 +1,17 @@
+const  juegos  =  ( prefijo ,  pushname )  =>  {
+    volver  '
+* Comandos De Juegos 👾 *
+ 
+$ { prefix } rankgay
+Conoce a el top de los mas gays del grupo
+$ { prefix } gay
+Conoce que tan gay eres 😎
+$ { prefix } bellezas
+Prueba tu suerte en el medidor de fan de cuties 😱
+_ * En futuras versiones colocare mas juegos:) * _
+     
+ву ѕнαη∂υу`
+
+}
+
+Exportaciones . juegos  =  juegos
