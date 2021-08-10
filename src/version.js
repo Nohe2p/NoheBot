1d64617204a2d2283f06333bdfc45c3c15279dc6
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:55:18 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -9,8 +9,7 @@ Si no funciona el comando * play o * play2 checa el blog del bot y mira la versi
 Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
 Cambiare la apikey pasando un mes o si veo muchos atercados o robos
 Si vez a alguien que robo mi bot comunicate conmigo con el comando * creador
-* BLOG DE SHANBOT *
-https://cutt.ly/jmSF2RN
+
 ву ѕнαη∂υу
 '
 
