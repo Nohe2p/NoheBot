8f4baf6261d777dba94cb5126da9bc8c1edd0c6f
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-09 19:25:29 -0500

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,79 @@
+const  ayuda  =  ( prefijo )  =>  {
+	volver  '
+⌜ * ѕнαηвσт ву ѕнαη∂υу * ⌟ 
+◉ * NUEVOS COMANDOS *
+○ $ { prefix } levelmenu
+Nivel para tus grupos
+○ $ { prefix } antimenu
+Su nombre lo dice todo (antienlace)
+○ $ { prefix } otaku
+Monas chinas
+○ $ { prefix } conversa
+Interactua con el bot
+◉ * MENUS NUEVOS *
+○ $ { prefix } juegos
+Divierte con tus amigos :)
+○ $ { prefix } desmenu
+Descargar musica y videos de YT
+○ $ { prefix } versión
+Conoce la versión de tu bot
+○ $ { prefix } welmenu
+Comandos de bienvedia a grupos
+◉ * PARA USAR EL BOT *
+Registrate con el comando $ { prefix } daftar y tu nombre
+◉ * RESUELVE TUS DUDAS *
+  ║
+  ╠ ○ $ { prefix } creador
+  ╚ Dudas o problemas aqui
+◉ * CREAR PEGATINAS *
+  ║
+  ╠ ○ $ { prefix } pegatina
+  ╠ ○ $ { prefix } attp
+  ╠ Mas un texto corto
+  ╠ ○ $ { prefix } stickergif
+  ╚ 6 segundos de video
+◉ * CONVERTIDORES *
+  ║
+  ╠ ○ $ { prefix } toimg
+  ╠ De pegatina a JPG
+  ╠ ○ $ { prefijo } tomp3
+  ╚ De video a MP3
+◉ * AUDIO *
+  ║
+  ╠ ○ $ { prefix } idioma
+  ╚ ○ $ { prefijo } tts es (mas texto)
+◉ * OTROS *
+  ║
+  ╠ ○ $ { prefix } wame
+  ╠ Enlace de Whatsapp
+  ╠ ○ $ { prefix } qrcode
+  ╚ Coloca un texto
+◉ * GRUPOS *
+  ║
+  ╠ ○ $ { prefix } closegc
+  ╠ Cerrar el grupo solo admins
+  ╠ ○ $ { prefix } opengc
+  ╠ Abrir grupo solo admins
+  ╠ ○ $ { prefix } menú inicial
+  ╠ Eliminar a un miembro 
+  ╠ ○ $ { prefix } promover
+  ╠ Dar admin a un miembro
+  ╠ ○ $ { prefix } degradar
+  ╠ Quitar el admin
+  ╠ ○ $ { prefix } linkgc
+  ╠ Enlace del grupo
+  ╠ ○ $ { prefix } todos
+  ╚ Nombra a todos los del grupo
+Para usar estas funciones el bot necesita admin
+  
+◉ * NSWF * 
+  ║
+  ╚ ○ $ { prefix } nsfwmenu
+Para activar los NSFW coloque el siguiente comando $ { prefix } nsfw 1 y para desactivar los NSFW coloque $ { prefix } nsfw 0
+No te olvides de seguirme en instagram flaco;)
+ву ѕнαη∂υу
+⌜ * ご き げ ん よ う:) * ⌟ 
+'
+}
+
+Exportaciones . ayuda  =  ayuda
