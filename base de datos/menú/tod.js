92597f78805e8db458acf186c3c83b59194409b6
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:55:42 -0500

    Create tod.js

---
+++ base de datos/menú/tod.js
@@ -0,0 +1,19 @@
+const  tod  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * TOD *
+  │
+  ├─ ❏ $ { prefix } trust
+  └─ ❏ $ { prefix } dare`
+}
+Exportaciones . tod  =  tod
