62d2cee9ef29d443383bb6ba86d2c61e2b268ec9
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:50:57 -0500

    Create owner.js

---
+++ base de datos/menú/owner.js
@@ -0,0 +1,23 @@
+ propietario  constante =  ( prefijo ,  propietarioBot ,  nombreBot )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * PROPIETARIO *
+  │
+  ├─ ❏ $ { prefix } setprefix
+  ├─ ❏ $ { prefijo } bloque
+  ├─ ❏ $ { prefijo } bc
+  ├─ ❏ $ { prefijo } bcgc
+  ├─ ❏ $ { prefix } clon
+  └─ ❏ $ { prefijo } clearall`
+}
+Exportaciones . propietario  =  propietario
