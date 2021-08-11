7df516dd00e960975048b924c8a5aadb771dfd88
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:42:32 -0500

    Create group.js

---
+++ base de datos/menú/group.js
@@ -0,0 +1,38 @@
+const  group  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * GRUPO *
+  │
+  ├─ ❏ $ { prefijo } opengc
+  ├─ ❏ $ { prefijo } closegc
+  ├─ ❏ $ { prefix } promover
+  ├─ ❏ $ { prefix } degradar
+  ├─ ❏ $ { prefix } tagall
+  ├─ ❏ $ { prefix } tagall2
+  ├─ ❏ $ { prefix } tagall3
+  ├─ ❏ $ { prefix } tagall4
+  ├─ ❏ $ { prefix } tagall5
+  ├─ ❏ $ { prefix } agregar
+  ├─ ❏ $ { prefijo } patada
+  ├─ ❏ $ { prefix } listadmins
+  ├─ ❏ $ { prefix } linkgroup
+  ├─ ❏ $ { prefix } dejar
+  ├─ ❏ $ { prefix } bienvenido
+  ├─ ❏ $ { prefijo } nsfw
+  ├─ ❏ $ { prefix } nivelación
+  ├─ ❏ $ { prefix } nivel
+  ├─ ❏ $ { prefix } eliminar
+  ├─ ❏ $ { prefijo } simih
+  └─ ❏ $ { prefix } ownergroup`
+}
+Exportaciones . grupo  =  grupo
