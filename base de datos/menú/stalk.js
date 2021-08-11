6e74137a16ddbd4f662c4b7baf933ef68fad1b28
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:53:13 -0500

    Create stalk.js

---
+++ base de datos/menú/stalk.js
@@ -0,0 +1,19 @@
+const  tallo  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * STALK *
+  │
+  ├─ ❏ $ { prefijo } tiktokstalk
+  └─ ❏ $ { prefijo } igstalk`
+}
+Exportaciones . tallo  =  tallo
