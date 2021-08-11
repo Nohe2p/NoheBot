8f197af370a1a2723c29b13f079d178495eeab02
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:55:03 -0500

    Create stickermaker.js

---
+++ base de datos/menú/stickermaker.js
@@ -0,0 +1,20 @@
+const  stickermaker  =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * FABRICANTE DE PEGATINAS *
+  │
+  ├─ ❏ $ { prefix } pegatina
+  ├─ ❏ $ { prefix } stickergif
+  └─ ❏ $ { prefijo } ttp`
+}
+Exportaciones . stickermaker  =  stickermaker
