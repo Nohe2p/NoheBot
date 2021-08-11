4e017deb99159ee49ac9f57d6c630f8c3e62d682
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:46:25 -0500

    Create islam.js

---
+++ base de datos/menú/islam.js
@@ -0,0 +1,18 @@
+const  islam  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * ISLAM *
+  │
+  └─ ❏ $ { prefijo } quran`
+}
+Exportaciones . islam  =  islam
