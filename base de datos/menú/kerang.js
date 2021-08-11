1ef38428268c82c7dc2e3af3c97a67da314c8d55
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:47:12 -0500

    Create kerang.js

---
+++ base de datos/menú/kerang.js
@@ -0,0 +1,21 @@
+const  kerang  =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * FABRICANTE *
+  │
+  ├─ ❏ $ { prefijo } apakah
+  ├─ ❏ $ { prefijo } kapankah
+  ├─ ❏ {$ prefijo } tasa
+  └─ ❏ $ { prefijo } bisakah`
+}
+Exportaciones . kerang  =  kerang
