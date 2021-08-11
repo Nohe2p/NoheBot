8c0cb8b2f5fb7afdc16bdc3aa7a6dc639d5bdd36
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:43:33 -0500

    Create help.js

---
+++ base de datos/menú/help.js
@@ -0,0 +1,47 @@
+const  help  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * MENÚ *
+  │
+  ├─ ❏ $ { prefix } logomakermenu
+  ├─ ❏ $ { prefix } menú creador de imágenes
+  ├─ ❏ $ { prefix } stickermakermenu
+  ├─ ❏ $ { prefix } menú de búsqueda
+  ├─ ❏ $ { prefijo } menú de educación
+  ├─ ❏ $ { prefijo } kerangmenu
+  ├─ ❏ $ { prefix } menú de descarga
+  ├─ ❏ $ { prefix } mememenu
+  ├─ ❏ $ { prefix } groupmenu
+  ├─ ❏ $ { prefijo } menú de sonido
+  ├─ ❏ $ { prefijo } menú de música
+  ├─ ❏ $ { prefijo } islammenu
+  ├─ ❏ $ { prefix } stalkmenu
+  ├─ ❏ $ { prefijo } wibumenu
+  ├─ ❏ $ { prefix } 18 + menú
+  ├─ ❏ $ { prefix } funmenu
+  ├─ ❏ $ { prefijo } todmenu
+  ├─ ❏ $ { prefix } menú de información
+  ├─ ❏ $ { prefix } stayonscreenmenu
+  ├─ ❏ $ { prefix } ownermenu
+  └─ ❏ $ { prefijo } otro menú
+◪ * OTRO *
+  │
+  ├─ ❏ $ { prefix } enviar
+  ├─ ❏ $ { prefijo } wame
+  ├─ ❏ $ { prefijo } virtex
+  ├─ ❏ $ { prefijo } qrcode
+  ├─ ❏ $ { prefix } temporizador
+  ├─ ❏ $ { prefijo } fml
+  └─ ❏ $ { prefijo } fml2`
+}
+Exportaciones . ayuda  =  ayuda
