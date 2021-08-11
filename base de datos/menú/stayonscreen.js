9fee1d875ec7acd1db9f01047c667ee107988492
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:54:23 -0500

    Create stayonscreen.js

---
+++ base de datos/menú/stayonscreen.js
@@ -0,0 +1,18 @@
+const  stayonscreen  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * MENÚ *
+  │
+  └─ ❏ $ { prefijo } afk`
+}
+Exportaciones . stayonscreen  =  stayonscreen
