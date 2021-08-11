9c17cf64ba6a17a4d451b4e38094f84bdac3b98d
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:44:15 -0500

    Create imagemaker.js

---
+++ base de datos/menú/imagemaker.js
@@ -0,0 +1,36 @@
+const  imagemaker  =  ( prefijo ,  botName ,  ownerName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * FABRICANTE DE IMÁGENES *
+  │
+  ├─ ❏ $ { prefijo } bpink
+  ├─ ❏ $ { prefix } snowwrite
+  ├─ ❏ $ { prefijo } 3dtext
+  ├─ ❏ $ { prefix } firetext
+  ├─ ❏ $ { prefijo } fallo
+  ├─ ❏ $ { prefijo } sombra
+  ├─ ❏ $ { prefijo } burnpaper
+  ├─ ❏ $ { prefix } café
+  ├─ ❏ $ { prefix } lovepaper
+  ├─ ❏ $ { prefijo } bloque de madera
+  ├─ ❏ $ { prefix } qowheart
+  ├─ ❏ $ { prefix } mutgrass
+  ├─ ❏ $ { prefix } undergocean
+  ├─ ❏ $ { prefix } tablas de madera
+  ├─ ❏ $ { prefijo } wolfmetal
+  ├─ ❏ $ { prefijo } metalictglow
+  ├─ ❏ $ { prefijo } 8 bits
+  ├─ ❏ $ { prefijo } herrypotter
+  └─ ❏ $ { prefix } quotemaker`
+}
+Exportaciones . creador de imágenes
