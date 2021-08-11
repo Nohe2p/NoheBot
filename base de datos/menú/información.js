fda293891983601fe26701d2418711381c81726f
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:45:42 -0500

    Create información.js

---
+++ base de datos/menú/información.js
@@ -0,0 +1,25 @@
+información  constante =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * INFORMACIÓN *
+  │
+  ├─ ❏ $ { prefix } bahasa
+  ├─ ❏ $ { prefijo } kodenegara
+  ├─ ❏ $ { prefijo } kbbi
+  ├─ ❏ $ { prefijo } fakta
+  ├─ ❏ $ { prefijo } infocuaca
+  ├─ ❏ $ { prefijo } infogempa
+  ├─ ❏ $ { prefijo } jadwaltvnow
+  └─ ❏ $ { prefix } covidcountry`
+}
+Exportaciones . información  =  información
