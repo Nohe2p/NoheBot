88df8bdc7eda54e12c66a347e1368631f08db076
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:51:38 -0500

    Create search.js

---
+++ base de datos/menú/search.js
@@ -0,0 +1,23 @@
+búsqueda  constante =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * Buscar *
+  │
+  ├─ ❏ $ { prefix } espera
+  ├─ ❏ $ { prefix } ytsearch
+  ├─ ❏ $ { prefix } trendtwit
+  ├─ ❏ $ { prefijo } wikien
+  ├─ ❏ $ { prefijo } wiki
+  └─ ❏ $ { prefijo } neonime`
+}
+Exportaciones . buscar  =  buscar
