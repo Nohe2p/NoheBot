81f0b1ed356b689525abfb48aef61860cc813c4a
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:50:03 -0500

    Create other.js

---
+++ base de datos/menú/other.js
@@ -0,0 +1,24 @@
+const  otro  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * OTRO *
+  │
+  ├─ ❏ $ { prefix } enviar
+  ├─ ❏ $ { prefijo } wame
+  ├─ ❏ $ { prefijo } virtex
+  ├─ ❏ $ { prefijo } qrcode
+  ├─ ❏ $ { prefix } temporizador
+  ├─ ❏ $ { prefijo } fml
+  └─ ❏ $ { prefijo } fml2`
+}
+Exportaciones . otro  =  otro
