53b2be63bc6fc4f20106c729ab71e3de237a2fa9
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:56:23 -0500

    Create wibu.js

---
+++ base de datos/menú/wibu.js
@@ -0,0 +1,26 @@
+const  wibu  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * WIBU *
+  │
+  ├─ ❏ $ { prefix } neonime
+  ├─ ❏ $ { prefijo } pokemon
+  ├─ ❏ $ { prefijo } loli
+  ├─ ❏ $ { prefijo } waifu
+  ├─ ❏ $ { prefix } randomanime
+  ├─ ❏ $ { prefix } husbu
+  ├─ ❏ $ { prefijo } husbu2
+  ├─ ❏ $ { prefix } espera
+  └─ ❏ $ { prefijo } nekonime`
+}
+Exportaciones . wibu  =  wibu
