de1854f69a01b4c69413a60aafc852718eb47d57
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:52:18 -0500

    Create sound.js

---
+++ base de datos/menú/sound.js
@@ -0,0 +1,18 @@
+const  sound  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * SONIDO *
+  │
+  └─ ❏ $ { prefijo } tts`
+}
+Exportaciones . sonido  =  sonido
