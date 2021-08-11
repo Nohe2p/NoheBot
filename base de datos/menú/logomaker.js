b4a148312b5afc899a2d2432c6f48e3573acf9c8
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:47:58 -0500

    Create logomaker.js

---
+++ base de datos/menú/logomaker.js
@@ -0,0 +1,27 @@
+const  logomaker  =  ( prefijo ,  botName ,  ownerName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * FABRICANTE DE LOGO *
+  │
+  ├─ ❏ $ { prefix } marvellogo
+  ├─ ❏ $ { prefijo } ninjalogo
+  ├─ ❏ $ { prefix } logowolf
+  ├─ ❏ $ { prefijo } logowolf2
+  ├─ ❏ $ { prefix } phlogo
+  ├─ ❏ $ { prefix } neonlogo
+  ├─ ❏ $ { prefix } neonlogo2
+  ├─ ❏ $ { prefix } lionlogo
+  ├─ ❏ $ { prefix } pubglogo
+  └─ ❏ $ { prefijo } jokerlogo`
+}
+Exportaciones . logomaker  =  logomaker
