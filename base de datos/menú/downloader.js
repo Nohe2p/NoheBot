f3f2eb533f40164fa72d5c727b9109f3e035a3e3
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:39:11 -0500

    Create downloader.js

---
+++ base de datos/menú/downloader.js
@@ -0,0 +1,21 @@
+descargador  const =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* BOT_NAME *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: OWNER_NAME
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * DESCARGADOR *
+  │
+  ├─ ❏ $ { prefix } pinterest
+  ├─ ❏ $ { prefijo } ytmp3
+  ├─ ❏ $ { prefijo } ytmp4
+  └─ ❏ $ { prefijo } tiktok`
+}
+Exportaciones . downloader  =  descargador
