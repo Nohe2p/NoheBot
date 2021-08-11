19f64220efc647dd95017a60d6d17c0aff139b9a
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:40:33 -0500

    Create fun.js

---
+++ base de datos/menú/fun.js
@@ -0,0 +1,25 @@
+const  fun  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * DIVERSIÓN *
+  │
+  ├─ ❏ $ { prefijo } alay
+  ├─ ❏ $ { prefijo } gantengcek
+  ├─ ❏ $ { prefijo } watak
+  ├─ ❏ $ { prefix } pasatiempo
+  ├─ ❏ $ { prefix } juego
+  ├─ ❏ $ { prefix } bucin
+  ├─ ❏ $ { prefix } trust
+  ├─ ❏ $ { prefix } dare
+  └─ ❏ $ { prefijo } simi`
+}
