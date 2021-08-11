5eead7cbab743c77cc8bc3f5fdd0f43e4e8a042d
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:48:36 -0500

    Create meme.js

---
+++ base de datos/menú/meme.js
@@ -0,0 +1,19 @@
+const  meme  =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * MEME *
+  │
+  ├─ ❏ $ { prefijo } meme
+  └─ ❏ $ { prefijo } memeindo`
+}
+Exportaciones . meme  =  meme
