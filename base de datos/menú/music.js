feaeadf25fc4de1e36b4287b8b29a36fd151bb53
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:49:18 -0500

    Create music.js

---
+++ base de datos/menú/music.js
@@ -0,0 +1,21 @@
+const  music  =  ( prefijo ,  ownerBot ,  botName )  =>  {
+        volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerBot }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * MÚSICA *
+  │
+  ├─ ❏ $ { prefix } jugar
+  ├─ ❏ $ { prefijo } joox
+  ├─ ❏ $ { prefijo } lirik
+  └─ ❏ $ { prefijo } acorde`
+}
+Exportaciones . musica  =  musica
