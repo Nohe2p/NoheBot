ac2bfcd202e56387489b6f82058e87a365d022da
Nohe2p <88568194+Nohe2p@users.noreply.github.com>
2021-08-11 11:39:54 -0500

    Create education.js

---
+++ base de datos/menú/education.js
@@ -0,0 +1,23 @@
+educación  constante =  ( prefijo ,  botName ,  ownerName )  =>  {
+	volver  '
+「* $ { BotName } *」
+◪ * INFO *
+  ❏ Prefijo: 「   $ { prefix }   」
+  ❏ Creador: $ { ownerName }
+◪ * ACERCA DE *
+  │
+  ├─ ❏ $ { prefix } info
+  ├─ ❏ $ { prefix } lista de bloqueo
+  ├─ ❏ $ { prefix } lista de chat
+  ├─ ❏ $ { prefijo } ping
+  └─ ❏ $ { prefix } informe de error
+◪ * EDUCACIÓN *
+  │
+  ├─ ❏ $ { prefijo } wiki
+  ├─ ❏ $ { prefijo } wikien
+  ├─ ❏ $ { prefijo } nulis
+  ├─ ❏ $ { prefijo } comillas
+  ├─ ❏ $ { prefijo } comillas2
+  └─ ❏ $ { prefijo } artinama`
+}
+Exportaciones . educación  =  educación
